5f8afe8c3ee8d192f071aa01b0c7f358d20de3c8
Aleksandr <soft@vhlv.ru>
2023-05-03 12:25:23 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="root"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.text {
+  font-family: 'Open Sans', Arial, Helvetica, sans-serif;
+  font-weight: 400;
+}
+</style>
