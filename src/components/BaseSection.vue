b597fdd9933bcf39df71de75a128ff700cfbf305
Aleksandr <soft@vhlv.ru>
2023-05-03 20:20:16 +0300

    feat: add BaseSection component

---
+++ src/components/BaseSection.vue
@@ -0,0 +1,21 @@
+<template>
+  <section
+    class="section"
+    :class="className"
+  >
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    className: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
